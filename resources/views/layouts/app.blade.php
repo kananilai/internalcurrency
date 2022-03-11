c2f27dd9db8c1d5fac5197abce00165ced532f61
willnilai <sara.will.nilai@gmail.com>
2022-03-11 21:04:52 +0900

    migrate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>アプリ</title>
+    <title>Thanks</title>
     <link rel="stylesheet" href="{{ asset('/assets/css/style.css')}}">
 </head>
 <body>
     <header class="header">
         <div class="app_name">
-            <h1><a href="{{ route('top') }}">アプリ名</a></h1>
+            <h1><a href="{{ route('top') }}">Thanks Message</a></h1>
         </div>
         <ul class="nav_list">
             <li>
